e9cb60b66a97abeefc69dbb1d4d5df68a530fa13
S1lentSt0rm <lutoshkinstep@gmail.com>
2023-08-29 21:10:01 +0300

    Game scene + localhost network
    Added movable player character controlled with WASD keys.
    Added simple field with borders.
    Playable on localhost.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+using UnityEngine.InputSystem;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    
+    private NetworkVariable<Vector2> movementDirection = 
+        new NetworkVariable<Vector2>(Vector2.zero,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
+
+    private Rigidbody2D _rigidbody2D;
+    private InputActions _inputActions;
+
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _inputActions = new InputActions();
+        _inputActions.Player.Enable();
+    }
+
+    void Update()
+    {
+        if (!IsOwner) return;
+        MoveServerRpc();
+        movementDirection.Value = _inputActions.Player.Movement.ReadValue<Vector2>();
+    }
+
+    [ServerRpc]
+    private void MoveServerRpc()
+    {
+        _rigidbody2D.velocity = movementDirection.Value * movementSpeed;
+    }
+    
+}
---
+++ Assets/Scripts/TestNetwork.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestNetwork : MonoBehaviour
+{
+    [SerializeField] private Button serverBtn;
+    [SerializeField] private Button hostBtn;
+    [SerializeField] private Button clientBtn;
+    private void Awake()
+    {
+        serverBtn.onClick.AddListener(() => NetworkManager.Singleton.StartServer());
+        hostBtn.onClick.AddListener(() => NetworkManager.Singleton.StartHost());
+        clientBtn.onClick.AddListener(() => NetworkManager.Singleton.StartClient());
+    }
+}
